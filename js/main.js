436ba779bf5dfa312d8af040df92c84a78ea70ec
Teresa Marban <teresamarbancastro@gmail.com>
2018-01-03 12:05:54 +0100

    faq added

--- js/main.js
+++ js/main.js
@@ -7,9 +7,18 @@ function showMenu (){
 
 $('.carousel').carousel();
 
+//Menu
 var show = document.querySelector('.burger');
 var list = document.querySelector('.dropdown-list');
 function showMenu (){
 	 list.classList.toggle('show');
 }
 show.addEventListener('click', showMenu);
+
+//Desplegables FAQ
+var showFaq = document.querySelectorAll('.questions-listed');
+var listFaq = document.querySelector('.questions-listed-answer');
+function showFaq (){
+	 list.classList.toggle('show');
+}
+show.addEventListener('click', showFaq);
